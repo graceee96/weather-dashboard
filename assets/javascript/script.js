e4eb6450813aee205a74f2bede123e547c562175
Grace Roh <grace@BIGMINE.attlocal.net>
2023-03-27 22:06:31 -0700

    format current weather section

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+//html elements
+
+
+//api links
+
+
+//global variables
+
+
+//function - render time on page
+
+
+//fetch - geocoding api
+
+
+//fetch - render weather onto page (might have to nest fetch within fetch) & create history button
+
+
+//fetch - render weather onto page when past searches list is clicked
